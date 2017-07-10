7e50bb06afd945e9e362590c95883b140a8a7005
Kevin Genus <kevin.genus@gmail.com>
2017-07-10 11:37:57 -0400

    Action Creators

--- client/components/MessagesList.js
+++ client/components/MessagesList.js
@@ -1,35 +1,46 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Message from './Message';
 import NewMessageEntry from './NewMessageEntry';
 import axios from 'axios';
-import store, { gotMessagesFromServer } from './store';
+import store, {gotMessagesFromServer} from '../store';
 
 export default class MessagesList extends Component {
 
-  constructor () {
-    super();
-    this.state = { messages: [] };
-  }
+    constructor() {
+        super();
+        this.state = store.getState();
+    }
 
-  componentDidMount () {
-    axios.get('/api/messages')
-      .then(res => res.data)
-      .then(messages => this.setState({ messages }));
-  }
 
-  render () {
 
-    const channelId = Number(this.props.match.params.channelId); // because it's a string "1", not a number!
-    const messages = this.state.messages;
-    const filteredMessages = messages.filter(message => message.channelId === channelId);
+    componentDidMount() {
+        axios.get('/api/messages')
+            .then(res => res.data)
+            .then(messages => {
+                const action = gotMessagesFromServer(messages);
+                store.dispatch(action);
+            });
 
-    return (
-      <div>
-        <ul className="media-list">
-          { filteredMessages.map(message => <Message message={message} key={message.id} />) }
-        </ul>
-        <NewMessageEntry />
-      </div>
-    );
-  }
+        store.subscribe(() => this.setState(store.getState()));
+    }
+
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+
+    render() {
+
+        const channelId = Number(this.props.match.params.channelId); // because it's a string "1", not a number!
+        const messages = this.state.messages;
+        const filteredMessages = messages.filter(message => message.channelId === channelId);
+
+        return (
+            <div>
+                <ul className="media-list">
+                    { filteredMessages.map(message => <Message message={message} key={message.id}/>) }
+                </ul>
+                <NewMessageEntry />
+            </div>
+        );
+    }
 }
